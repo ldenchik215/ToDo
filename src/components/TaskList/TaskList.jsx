a3032515e0765eff9f56bf925f4779f128182614
unknown <ldenchik215@gmail.com>
2025-03-28 12:34:31 +0700

    fix eslint error

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -4,17 +4,15 @@ import React from 'react'
 import Task from '../Task/Task'
 
 export default function TaskList({ filtered, taskEdit, onClickDelete, toggleTaskState }) {
-  const tasks = filtered?.map((task) => {
-    return (
-      <Task
-        key={task.id}
-        {...task}
-        taskEdit={taskEdit}
-        toggleTaskState={toggleTaskState}
-        onClickDelete={() => onClickDelete(task.id)}
-      />
-    )
-  })
+  const tasks = filtered?.map((task) => (
+    <Task
+      key={task.id}
+      {...task}
+      taskEdit={taskEdit}
+      toggleTaskState={toggleTaskState}
+      onClickDelete={() => onClickDelete(task.id)}
+    />
+  ))
 
   return <ul className="todo-list">{tasks}</ul>
 }
